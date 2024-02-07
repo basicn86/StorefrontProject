3a63310653cebbe5a0ab7fe1df7a6e3be6c6d4fc
basicn86 <basicn86@gmail.com>
2024-02-07 15:32:24 -0500

    Add project files.

---
+++ StorefrontProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StorefrontProject.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ StorefrontProject/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace StorefrontProject.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
