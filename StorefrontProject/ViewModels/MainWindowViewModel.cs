2921e8601495a9bb0cf220d47883ec0cadad4b2d
basicn86 <basicn86@gmail.com>
2024-02-07 17:04:59 -0500

    Added catalog view and more buttons

--- StorefrontProject/ViewModels/MainWindowViewModel.cs
+++ StorefrontProject/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,15 @@ namespace StorefrontProject.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private ViewModelBase? mainContent;
+        public ViewModelBase? MainContent {
+            get => mainContent;
+            set => this.RaiseAndSetIfChanged(ref mainContent, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            MainContent = new CatalogViewModel();
+        }
     }
 }
