8a29eb6839e64f57127aa14e4d110cc3d8ff2c30
Nedim Basic <basicn86@gmail.com>
2024-02-10 18:22:22 -0500

    Added catalog and example foods

--- StorefrontProject/ViewModels/CatalogViewModel.cs
+++ StorefrontProject/ViewModels/CatalogViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 
 namespace StorefrontProject.ViewModels
 {
     public class CatalogViewModel : ViewModelBase
     {
-
+        public ObservableCollection<Bitmap> TestProducts { get; set; } = new ObservableCollection<Bitmap> { new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\apple.jpg"),
+        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\banana.jpg"),
+        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\carrot.jpg"),
+        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\durian.webp"),
+        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\eggplant.webp"),
+        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\figs.jpg")};
     }
 }
