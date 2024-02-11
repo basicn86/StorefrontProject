b299e3e0e64a7d91cec1d37f7b78e26f9fc0ce6e
Nedim Basic <basicn86@gmail.com>
2024-02-10 19:47:13 -0500

    Added Database

--- StorefrontProject/ViewModels/CatalogViewModel.cs
+++ StorefrontProject/ViewModels/CatalogViewModel.cs
@@ -1,20 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
+using StorefrontProject.Models;
 
 namespace StorefrontProject.ViewModels
 {
     public class CatalogViewModel : ViewModelBase
     {
-        public ObservableCollection<Bitmap> TestProducts { get; set; } = new ObservableCollection<Bitmap> { new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\apple.jpg"),
-        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\banana.jpg"),
-        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\carrot.jpg"),
-        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\durian.webp"),
-        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\eggplant.webp"),
-        new Bitmap("C:\\Users\\Nedim\\source\\repos\\StorefrontProject\\StorefrontProject\\Assets\\figs.jpg")};
+        public ObservableCollection<CatalogItemViewModel> CatalogItems { get; set; }
+
+        private StoreDbContext context = new StoreDbContext();
+        public CatalogViewModel()
+        {
+            CatalogItems = new ObservableCollection<CatalogItemViewModel>();
+            context.ResetDatabase();
+
+            foreach (var product in context.Products)
+            {
+                if (product.ProductImage == null)
+                {
+                    continue;
+                }
+
+                using (MemoryStream ms = new MemoryStream(product.ProductImage))
+                {
+                    CatalogItemViewModel item = new CatalogItemViewModel(product.Name, product.Price, new Bitmap(ms));
+                    CatalogItems.Add(item);
+                }
+            }
+        }
     }
 }
