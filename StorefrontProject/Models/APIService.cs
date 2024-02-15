8be71cbfd6a5077fe660d3d10423761692117f2a
Nedim Basic <basicn86@gmail.com>
2024-02-14 21:02:46 -0500

    Added API service interface and class

---
+++ StorefrontProject/Models/APIService.cs
@@ -0,0 +1,21 @@
+﻿using StorefrontProject.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorefrontProject.Models
+{
+    public class APIService : IAPIService
+    {
+        //get products from the database
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            using (StoreDbContext context = new StoreDbContext("store.db"))
+            {
+                return await Task.Run(() => (from p in context.Products select p).Take(10));
+            }
+        }
+    }
+}
---
+++ StorefrontProject/Models/Interfaces/IAPIService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorefrontProject.Models.Interfaces
+{
+    public interface IAPIService
+    {
+        public Task<IEnumerable<Product>> GetProductsAsync();
+    }
+}
