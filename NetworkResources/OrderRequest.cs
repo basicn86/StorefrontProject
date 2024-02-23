a3b339c3c78bf2ee9c5f917be3f7c70f50359334
Nedim Basic <basicn86@gmail.com>
2024-02-22 23:24:27 -0500

    Added some comments

--- NetworkResources/OrderRequest.cs
+++ NetworkResources/OrderRequest.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace NetworkResources
 {
+    //This is usually sent from the client to the server to place an order
     public record OrderRequest
     {
         //dictionary of int to ints
         public Dictionary<int, int> OrderItems { get; init; } = new Dictionary<int, int>();
         //total price of the order as a decimal
+        //Important Note: this is to verify that the client and server agree on the total price
         public decimal TotalPrice { get; init; }
     }
 }
