412d998c98c4fe9c2826224fff48dce5d0efb8ce
Nedim Basic <basicn86@gmail.com>
2024-02-25 21:16:18 -0500

    Added more items

--- StorefrontAPI/MemoryDatabase.cs
+++ StorefrontAPI/MemoryDatabase.cs
@@ -8,9 +8,11 @@
         //store list of products in memory for now
         public static List<NetworkResources.Product> Products = new List<NetworkResources.Product>
         {
-            new NetworkResources.Product { Id = 1, Name = "Walnuts", Price = 50.00m },
+            new NetworkResources.Product { Id = 1, Name = "Walnuts", Price = 60.00m },
             new NetworkResources.Product { Id = 2, Name = "Pineapples", Price = 20.00m },
-            new NetworkResources.Product { Id = 3, Name = "Coconuts", Price = 30.00m }
+            new NetworkResources.Product { Id = 3, Name = "Coconuts", Price = 30.00m },
+            new NetworkResources.Product { Id = 4, Name = "Peanuts", Price = 40.00m },
+            new NetworkResources.Product { Id = 5, Name = "Pistachios", Price = 60.00m }
         };
     }
 }
