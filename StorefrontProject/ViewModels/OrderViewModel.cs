b9ce733090d16ad760a3eef5c0242406867feaa9
Nedim Basic <basicn86@gmail.com>
2024-02-25 20:26:40 -0500

    Added Order View Model

---
+++ StorefrontProject/ViewModels/OrderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NetworkResources;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorefrontProject.ViewModels
+{
+    public class OrderViewModel
+    {
+        //order ID. This can be set to 0 on the client side. When placing an order, the server will assign a unique ID to the order, and disregard the client's ID.
+        public int Id { get; set; }
+        //total price of the order.
+        public decimal TotalPrice { get; set; }
+        public string TotalPriceString => TotalPrice.ToString("C");
+        //list of order items.
+        public ObservableCollection<OrderItem> OrderItems { get; set; }
+
+        public OrderViewModel()
+        {
+            OrderItems = new ObservableCollection<OrderItem>();
+        }
+    }
+}
--- StorefrontProject/ViewModels/OrdersViewModel.cs
+++ StorefrontProject/ViewModels/OrdersViewModel.cs
@@ -12,12 +12,12 @@ namespace StorefrontProject.ViewModels
     public class OrdersViewModel : ViewModelBase
     {
         //observable collection of orders
-        public ObservableCollection<NetworkResources.Order> OrderList { get; set; }
+        public ObservableCollection<OrderViewModel> OrderList { get; set; }
 
         public OrdersViewModel()
         {
             //initialize the observable collection
-            OrderList = new ObservableCollection<NetworkResources.Order>();
+            OrderList = new ObservableCollection<OrderViewModel>();
             //get the orders from the server
             _ = GetOrders();
         }
@@ -33,7 +33,16 @@ namespace StorefrontProject.ViewModels
             //put the orders in the observable collection
             foreach (var order in orders)
             {
-                OrderList.Add(order);
+                //Convert the order to an OrderViewModel
+                var orderViewModel = new OrderViewModel
+                {
+                    Id = order.Id,
+                    TotalPrice = order.TotalPrice,
+                    OrderItems = new ObservableCollection<NetworkResources.OrderItem>(order.OrderItems)
+                };
+
+                //add the order to the observable collection
+                OrderList.Add(orderViewModel);
             }
         }
     }
