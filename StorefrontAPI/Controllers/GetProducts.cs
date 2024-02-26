aca5bd68e8d14e5f3714641585e82518fbb2d254
Nedim Basic <basicn86@gmail.com>
2024-02-25 21:21:09 -0500

    Fixed GetProducts

--- StorefrontAPI/Controllers/GetProducts.cs
+++ StorefrontAPI/Controllers/GetProducts.cs
@@ -10,16 +10,8 @@ namespace StorefrontAPI.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            //list of products
-            List <NetworkResources.Product> products = new List<NetworkResources.Product>();
-
-            //add three fake products
-            products.Add(new NetworkResources.Product { Id = 1, Name = "Walnuts", Price = 50.00m });
-            products.Add(new NetworkResources.Product { Id = 2, Name = "Pineapples", Price = 20.00m });
-            products.Add(new NetworkResources.Product { Id = 3, Name = "Coconuts", Price = 30.00m });
-
-            //return the list of products in a JSON response
-            return Ok(products);
+            //return the list of products from the memory database
+            return Ok(MemoryDatabase.Products);
         }
     }
 }
